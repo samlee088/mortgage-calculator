a31a7842b6e9d90f21a5721b2966a8b935b9cc58
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-02-24 17:08:54 -0800

    initial set up -added in npm zod and shadcn -created calculator component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Calculator from "@/components/Calculator";
+
 export default function Home() {
   return (
     <main className="">
       <h1>Mortgage calculator</h1>
+      <Calculator />
     </main>
   );
 }
